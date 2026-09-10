85ccd1da3eea4dab0700870d05f88584e5e617db
Gal Yedidovich <84014945+Gal-Yedidovich-iSrc@users.noreply.github.com>
2026-09-10 15:06:05 +0300

    Update minimum os version to 15

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
   name: "LevelPlay-Swift-Package",
-  platforms: [.iOS(.v12)],
+  platforms: [.iOS(.v15)],
   products: [
     .library(name: "UnityMediationSDK", targets: ["LPSPM"]),
   ],
